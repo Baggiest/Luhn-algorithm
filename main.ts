2f6ade047bcb7800df9b3772181411acdf9f9f81
mrbaggiebug <mani.ehsanisohi@gmail.com>
2022-07-14 15:19:11 +0430

    oops

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 
 import evaluate from './algorithm'
-const cc: string = '4539-4794-2564-9801' // 4539479425649801 to test valid
+const cc: string = '4539479425649801' // 4539479425649801 to test valid
 
 async function bootstrap() {
     let result = await evaluate(cc)
